b349c314bb8d8dc491cc3048b7383be69e54c8f7
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-08-25 13:21:19 -0300

    implementado html, css, logica do tabuleiro

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,3 +1,4 @@
+import { JogoDaVelhaService } from './shared/jogo-da-velha.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jogodaVelhaService: JogoDaVelhaService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.jogodaVelhaService.inicializar();
+  }
+  /**retorna se a tela de inicio deve ser exibida */
+  get showInicio():boolean{
+    return this.jogodaVelhaService.showInicio;
+  }
+  /**retorna se o tabuleiro deve ser exibido */
+  get showTabuleiro():boolean{
+    return this.jogodaVelhaService.showTabuleiro;
+  }
+  /**retorna se a tela de final deve ser exibida */
+  get showFinal():boolean{
+    return this.jogodaVelhaService.showFinal;
+  }
+  /**inicializa os dados de um novo jogo */
+  iniciarJogo():void{
+    this.jogodaVelhaService.iniciarJogo();
+  }
+  /**realiza uma jogada o clicar no tabuleiro */
+  jogar(posX: number, posY:number): void{
+    this.jogodaVelhaService.jogar(posX, posY);
+  }
+  /**retorna se a peça X deve ser exibida */
+  exibirX(posX:number, posY:number):boolean{
+    return this.jogodaVelhaService.exibirX(posX,posY);
+  }
+  /**retorna se a peça 0 deve ser exibida */
+  exibirO(posX:number, posY:number):boolean{
+    return this.jogodaVelhaService.exibirO(posX,posY);
+  }
+  /**retorna se a marcacao de vitoria deve ser exibida */
+  exibirVitoria(posX:number, posY:number):boolean{
+    return this.jogodaVelhaService.exibirVitoria(posX,posY);
   }
 
 }
